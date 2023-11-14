05304ee3b7017f44d237138649c487b6b320d1a6
pappik <pappik@gmail.com>
2023-11-13 20:14:30 -0500

    global code for isolated resources

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -4,26 +4,38 @@ default_vpc_id = "vpc-04c71a590ec8a398c"
 vpc = {
   main = {
     cidr_block = "10.0.0.0/16"
-  }
-}
-
-subnets = {
-  public = {
-    name              = "public"
-    vpc_name          = "main"
-    cidr_block        = ["10.0.0.0/24" , "10.0.1.0/24"]
     availability_zone = ["us-east-1a" , "us-east-1b"]
-  }
+    subnets = {
+      public = {
+        name              = "public"
+        cidr_block        = ["10.0.0.0/24" , "10.0.1.0/24"]
+        internet_gw       = "true"
+        create_nat_gw     = true
+      }
 
-#  web = {
-#
-#  }
-#
-#  app = {
-#
-#  }
-#
-#  db = {
-#
-#  }
+      web = {
+        name              = "public"
+        cidr_block        = ["10.0.2.0/24" , "10.0.3.0/24"]
+        nat_gw            = "false"
+
+      }
+
+      app = {
+        name              = "public"
+        cidr_block        = ["10.0.4.0/24" , "10.0.5.0/24"]
+        nat_gw            = "false"
+
+
+      }
+
+      db = {
+        name              = "public"
+        cidr_block        = ["10.0.6.0/24" , "10.0.7.0/24"]
+        nat_gw            = "false"
+
+
+      }
+    }
+  }
 }
+
--- main.tf
+++ main.tf
@@ -3,8 +3,10 @@ module "vpc" {
   env = var.env
   default_vpc_id = var.default_vpc_id
 
-  for_each = var.vpc
-  cidr_block           = each.value.cidr_block
+  for_each    = var.vpc
+  cidr_block  = each.value.cidr_block
+  subnets     = each.value.subnets
+  availability_zone = each.value.availability_zone
 }
 
 module "subnets" {
@@ -14,10 +16,17 @@ module "subnets" {
 
 
 
-  for_each   = var.subnets
-  cidr_block = each.value.cidr_block
-  availability_zone = each.value.availability_zone
-  name = each.value.name
-  vpc_id = lookup(lookup(module.vpc, each.value.vpc_name , null ), "vpc_id" , null)
+  for_each                  = var.subnets
+  cidr_block                = each.value.cidr_block
+  availability_zone         = each.value.availability_zone
+  name                      = each.value.name
+  vpc_id                    = lookup(lookup(module.vpc, each.value.vpc_name , null ), "vpc_id" , null)
   vpc_peering_connection_id = lookup(lookup(module.vpc, each.value.vpc_name , null ), "vpc_peering_connection_id" , null)
+  internet_gw               = lookup(each.value, "internet_gw", false )
+  nat_gw                    = lookup(each.value, "nat_gw", false )
+
 }
+
+#output "subnet_ids" {
+#  value = lookup(lookup(module.subnets, "public", null ), "subnet_ids", null )
+#}
