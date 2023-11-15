97f09f7f78b9c9eb3efff9215133fac1e13033f8
pappik <pappik@gmail.com>
2023-11-15 18:52:58 -0500

    documentdb

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ module "rabbitmq" {
   for_each       = var.rabbitmq
   subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnet_ids", null), each.value.subnets_name, null), "subnet_ids", null)
   vpc_id         = lookup(lookup(module.vpc, each.value.vpc_name, null ), "vpc_id", null )
-  allow_cidr     = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null ), "private_subnets", null ), "app", null), "cidr_block", null)
+  allow_cidr     = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null ), "private_subnets", null ), "app", null), "cidr_block", null)
   engine_type    = each.value.engine_type
   engine_version = each.value.engine_version
   host_instance_type = each.value.host_instance_type
@@ -71,7 +71,7 @@ module "alb" {
   for_each   = var.alb
   subnet_ids = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), each.value.subnets_type, null), each.value.subnets_name, null), "subnet_ids", null)
   vpc_id     = lookup(lookup(module.vpc, each.value.vpc_name, null ), "vpc_id", null )
-  allow_cidr = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null ), "private_subnets", null ), "app", null), "cidr_block", null)
+  allow_cidr     = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null ), "private_subnets", null ), "app", null), "cidr_block", null)
   subnets_name = each.value.subnets_name
   internal     = each.value.internal
 }
@@ -83,7 +83,7 @@ module "apps" {
   for_each   = var.apps
   subnet_ids = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), each.value.subnets_type, null), each.value.subnets_name, null), "subnet_ids", null)
   vpc_id     = lookup(lookup(module.vpc, each.value.vpc_name, null ), "vpc_id", null )
-  allow_cidr = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null ), each.value.allow_cidr_subnets_type, null ), each.value.allow_cidr_subnets_name, null), "cidr_block", null)
+  allow_cidr = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null ), each.value.allow_cidr_subnets_type, null ), each.value.allow_cidr_subnets_name, null), "cidr_block", null)
   component  = each.value.component
   app_port   = each.value.app_port
   max_size                  = each.value.max_size
